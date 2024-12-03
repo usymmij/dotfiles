ca776627ddb11b346b9c9824781013b1aed3db06
usymmij <usymmij@gmail.com>
2024-12-03 09:53:32 -0500

    tabs to double space

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -710,9 +710,9 @@ vim.api.nvim_create_user_command("Cpshot", "!mkdir imgs && cpshot imgs", {});
 vim.g.netrw_liststyle = 3;
 
 -- set tabstops
-vim.opt.tabstop = 4;
-vim.opt.shiftwidth = 4;
-vim.opt.softtabstop = 4;
+vim.opt.tabstop = 2;
+vim.opt.shiftwidth = 2;
+vim.opt.softtabstop = 2;
 vim.opt.expandtab = true;
 vim.opt.autoindent = true;
 
