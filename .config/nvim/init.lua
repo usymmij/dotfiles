e5467e9b2cc45f9299e7a26b0afc0c5c84a1fe96
usymmij <usymmij@gmail.com>
2025-02-13 16:41:22 -0500

    add typst lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -540,22 +540,26 @@ local on_attach = function(_, bufnr)
 end
 
 -- document existing key chains
-require('which-key').register {
-    ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-    ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-    ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-    ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-    ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-    ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-    ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-    ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+require('which-key').add {
+    { "<leader>c",  group = "[C]ode" },
+    { "<leader>c_", hidden = true },
+    { "<leader>d",  group = "[D]ocument" },
+    { "<leader>d_", hidden = true },
+    { "<leader>g",  group = "[G]it" },
+    { "<leader>g_", hidden = true },
+    { "<leader>h",  group = "Git [H]unk" },
+    { "<leader>h_", hidden = true },
+    { "<leader>r",  group = "[R]ename" },
+    { "<leader>r_", hidden = true },
+    { "<leader>s",  group = "[S]earch" },
+    { "<leader>s_", hidden = true },
+    { "<leader>t",  group = "[T]oggle" },
+    { "<leader>t_", hidden = true },
+    { "<leader>w",  group = "[W]orkspace" },
+    { "<leader>w_", hidden = true },
+    { "<leader>",   group = "VISUAL <leader>", mode = "v" },
+    { "<leader>h",  desc = "Git [H]unk",       mode = "v" },
 }
--- register which-key VISUAL mode
--- required for visual <leader>hs (hunk stage) to work
-require('which-key').register({
-    ['<leader>'] = { name = 'VISUAL <leader>' },
-    ['<leader>h'] = { 'Git [H]unk' },
-}, { mode = 'v' })
 
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
@@ -586,6 +590,7 @@ local servers = {
             -- diagnostics = { disable = { 'missing-fields' } },
         },
     },
+    tinymist = {}
 }
 
 -- Setup neovim lua configuration
