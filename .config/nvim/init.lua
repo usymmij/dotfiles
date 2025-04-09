6463d1822db8fb9a3f403c5e07c4af35b073b899
usymmij <usymmij@gmail.com>
2025-04-09 02:12:36 -0400

    fix calc no conda issue

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -284,7 +284,15 @@ require('lazy').setup({
         'vyfor/cord.nvim',
         build = ':Cord update',
         -- opts = {}
-    }
+    },
+    --{
+    --    "kiyoon/jupynium.nvim",
+    --    -- build = "pip3 install --user .",
+    --    -- build = "uv pip install . --python=$HOME/.virtualenvs/jupynium/bin/python",
+    --    build = "conda run --no-capture-output -n jupynium pip install .",
+    --},
+    "rcarriga/nvim-notify",   -- optional
+    "stevearc/dressing.nvim", -- optional, UI for :JupyniumKernelSelect
 }, {})
 
 
