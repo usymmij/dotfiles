168a21f69ef0d2e5cd92bbfb17b8d958c961f695
usymmij <usymmij@gmail.com>
2025-09-15 19:06:39 -0400

    images in markdown

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -708,6 +708,13 @@ require('lazy').setup({
             local rend_md = require 'render-markdown'
         end,
     },
+    {
+        '3rd/image.nvim',
+        build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+        opts = {
+            processor = 'magick_cli',
+        },
+    },
 
     {
         'iamcco/markdown-preview.nvim',
